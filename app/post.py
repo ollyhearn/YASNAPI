6ea20589a118e837d4952540699d7ce0b17a4bda
Olly <artemkek0519@gmail.com>
2022-10-22 18:03:15 +0300

    /profile section updates and new /post section

---
+++ app/post.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, jsonify, session, make_response
+from db.models import db, PostModel
+from safety.token_validation import token_required
+from safety.key import secret_key
+import jwt
+from datetime import datetime, timedelta
+
+post_b = Blueprint("post", __name__)
+
+@post_b.route("/", methods=["GET"])
+def index():
+	postid = request.args.get("postid", default=None, type=int)
+	if postid:
+		post = PostModel.query.filter_by(id=postid).first()
+		if post:
+			return jsonify(post.as_dict()), 200
+		else:
+			return jsonify({'Message': 'Post not found'}), 403
+	return "OK", 200
+
+@token_required
+@post_b.route("/newpost", methods=["POST"])
+def newpost():
+	text = request.form.get('text', None)
+	title = request.form.get('title', None)
+	if not text:
+		return jsonify({'Message': 'Provide text at least'}), 403
+	data = jwt.decode(request.headers.get('token'), secret_key, algorithms="HS256")
+	new_post = PostModel(text=text, owner=data['id'], title=title)
+	db.session.add(new_post)
+	db.session.commit()
+	return jsonify({'Message': 'Post created'}), 200
+
+
